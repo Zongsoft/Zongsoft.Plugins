c6d8342c8d5159b349802722db46d6d2ae5ee142
Popeye Zhong <zongsoft@gmail.com>
2016-11-24 17:28:13 +0800

    feat: 重构关于终端程序的功能模块。 :tractor: :tennis:

---
+++ src/Terminals/ApplicationContext.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2013 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Plugins.
+ *
+ * Zongsoft.Plugins is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Plugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Plugins; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.IO;
+using System.Reflection;
+
+using Zongsoft.Plugins;
+
+namespace Zongsoft.Terminals.Plugins
+{
+	public class ApplicationContext : Zongsoft.Plugins.PluginApplicationContext
+	{
+		#region 静态变量
+		public new readonly static ApplicationContext Current = new ApplicationContext();
+		#endregion
+
+		#region 成员字段
+		private Zongsoft.Options.Configuration.OptionConfiguration _configuration;
+		#endregion
+
+		#region 私有构造
+		private ApplicationContext() : base("Zongsoft.Terminals.Plugins")
+		{
+			Zongsoft.ComponentModel.ApplicationContextBase.Current = this;
+		}
+		#endregion
+
+		#region 重写方法
+		public override Zongsoft.Options.Configuration.OptionConfiguration Configuration
+		{
+			get
+			{
+				if(_configuration == null)
+				{
+					string filePaht = Path.Combine(this.ApplicationDirectory, Assembly.GetEntryAssembly().GetName().Name) + ".option";
+
+					if(File.Exists(filePaht))
+						_configuration = Options.Configuration.OptionConfiguration.Load(filePaht);
+					else
+						_configuration = new Options.Configuration.OptionConfiguration(filePaht);
+				}
+
+				return _configuration;
+			}
+		}
+
+		protected override IWorkbenchBase CreateWorkbench(string[] args)
+		{
+			PluginTreeNode node = this.PluginContext.PluginTree.Find(this.PluginContext.Settings.WorkbenchPath);
+
+			if(node != null && node.NodeType == PluginTreeNodeType.Builtin)
+				return base.CreateWorkbench(args);
+
+			return new Workbench(this);
+		}
+		#endregion
+	}
+}
---
+++ src/Terminals/Workbench.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2010-2013 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Plugins.
+ *
+ * Zongsoft.Plugins is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Plugins is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Plugins; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+
+using Zongsoft.Plugins;
+using Zongsoft.Services;
+
+namespace Zongsoft.Terminals.Plugins
+{
+	public class Workbench : Zongsoft.Plugins.WorkbenchBase
+	{
+		#region 成员字段
+		private TerminalCommandExecutor _executor;
+		#endregion
+
+		#region 构造函数
+		public Workbench(PluginApplicationContext applicationContext) : base(applicationContext)
+		{
+			_executor = CommandExecutor.Default as TerminalCommandExecutor;
+		}
+		#endregion
+
+		#region 公共属性
+		public TerminalCommandExecutor Executor
+		{
+			get
+			{
+				return _executor;
+			}
+		}
+		#endregion
+
+		#region 打开方法
+		protected override void OnStart(string[] args)
+		{
+			var executor = this.Executor;
+
+			if(executor == null)
+				throw new InvalidOperationException("The command executor is null.");
+
+			//调用基类同名方法
+			base.OnStart(args);
+
+			//激发“Opened”事件
+			this.OnOpened(EventArgs.Empty);
+
+			//启动命令运行器
+			executor.Run();
+
+			//关闭命令执行器
+			this.Close();
+		}
+		#endregion
+	}
+}
